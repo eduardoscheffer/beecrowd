f69920774d4ce09cf03cea3aa0cd14ec726f4723
Eduardo Scheffer <eduardoscheffer91@gmail.com>
2023-08-23 21:40:19 -0300

    beecrowd 1052 Mes

---
+++ Iniciante/1052-Mes.java
@@ -0,0 +1,29 @@
+// import java.io.IOException;
+// import java.time.LocalDate;
+// import java.time.format.DateTimeFormatter;
+// import java.util.Locale;
+// import java.util.Scanner;
+ 
+// /**
+//  * IMPORTANT: 
+//  *      O nome da classe deve ser "Main" para que a sua solução execute
+//  *      Class name must be "Main" for your solution to execute
+//  *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+//  */
+// public class Main {
+ 
+//     public static void main(String[] args) throws IOException {
+ 
+//         Locale.setDefault((Locale.US));
+//         Scanner sc = new Scanner(System.in);
+//         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM");
+
+//         LocalDate data = LocalDate.of(2023, sc.nextInt(), 1);
+//         System.out.println(data.format(formatter));
+
+
+//         sc.close();
+ 
+//     }
+ 
+// }
