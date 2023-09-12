d72291f500c86d26b8eeb80eb301cc7af37f2395
Eduardo Scheffer <eduardoscheffer91@gmail.com>
2023-09-11 21:16:53 -0300

    beecrowd 1060-NumerosPares

---
+++ Iniciante/1060-NumerosPares.java
@@ -0,0 +1,30 @@
+// import java.io.IOException;
+// import java.util.*;
+ 
+// /**
+//  * IMPORTANT: 
+//  *      O nome da classe deve ser "Main" para que a sua solução execute
+//  *      Class name must be "Main" for your solution to execute
+//  *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+//  */
+// public class Main {
+ 
+//     public static void main(String[] args) throws IOException {
+ 
+//         Locale.setDefault((Locale.US));
+//         Scanner sc = new Scanner(System.in);
+
+//         double count = 0;
+//         for (int i = 1; i <= 6 ; i++) {
+//             if (sc.nextDouble() > 0) {
+//                 count++;
+//             }
+//         }
+
+//         System.out.println(String.format("%.0f", count) + " valores positivos");
+
+//         sc.close();
+ 
+//     }
+ 
+// }
