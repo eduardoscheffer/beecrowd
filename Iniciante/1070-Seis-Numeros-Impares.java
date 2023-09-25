70e958628f6272fedc8898bf98bc2fec6121f895
Eduardo Scheffer <eduardoscheffer91@gmail.com>
2023-09-25 19:18:44 -0300

    beecrowd 1070 Seis Números Impares

---
+++ Iniciante/1070-Seis-Numeros-Impares.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.util.*;
+ 
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        Locale.setDefault((Locale.US));
+        Scanner sc = new Scanner(System.in);
+
+        int X = sc.nextInt();
+
+        for (int i = 0; i < 12; i++) {
+
+            if (X % 2 != 0) {
+                System.out.println(X);
+            }
+            X++;
+        }
+
+        sc.close();
+ 
+    }
+ 
+}
