e86f66bb7228a59d64bcc0b150b0c59bbac2c75e
Eduardo Scheffer <107814795+eduardoscheffer@users.noreply.github.com>
2023-10-11 17:13:00 -0300

    beecrowd 1134-Tipo-de-Combustivel.java

---
+++ Iniciante/1134-Tipo-de-Combustivel.java
@@ -0,0 +1,46 @@
+import java.io.IOException;
+import java.util.*;
+ 
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        Locale.setDefault((Locale.US));
+        Scanner sc = new Scanner(System.in);
+        
+        int n, alcool = 0, gasolina = 0, diesel = 0;
+        
+        do {
+            
+            n = sc.nextInt();
+            switch(n) {
+                case 1:
+                    alcool++;
+                    break;
+                case 2:
+                    gasolina++;
+                    break;
+                case 3:
+                    diesel++;
+                    break;
+            }
+            
+        } while(n != 4);
+        
+        System.out.println("MUITO OBRIGADO");
+        System.out.println("Alcool: " + alcool);
+        System.out.println("Gasolina: " + gasolina);
+        System.out.println("Diesel: " + diesel);
+        
+        
+        sc.close();
+ 
+    }
+ 
+}
